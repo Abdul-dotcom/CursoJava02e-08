90bc411f0da9811884183318bf04653ad0107eef
Abdul-dotcom <Abdulrazak521.ar@gmail.com>
2019-12-22 21:18:12 +0200

    Diferente resolucao do exercicio 1 da ficha

--- CursoJava02e-08/src/metodos/Ex1/ContaAPrazo.java
+++ CursoJava02e-08/src/metodos/Ex1/ContaAPrazo.java
@@ -5,46 +5,24 @@ package metodos.Ex1;
  * @author _AlyyRazack
  */
 public class ContaAPrazo {
-    private String nome;
-    private double saldo;
-
-    public ContaAPrazo() {
+    String proprietario;
+     double saldo;
+    
+    
+    //metodo  q faz o levantamento
+    boolean levantamento(double valor){
+        if (this.saldo<valor) {
+            return false;
+        } else {
+            this.saldo = this.saldo - valor;
+            return true;
+        }
     }
-
-    public ContaAPrazo(String nome, double saldo) {
-        this.nome = nome;
-        this.saldo = saldo;
+    //Deposito
+    void deposita (double valor){
+        this.saldo += valor ;// this.saldo = saldo + valor;
     }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public double getSaldo() {
-        return saldo;
-    }
-
-    public void setSaldo(double saldo) {
-        this.saldo = saldo;
-    }
-    public  double definirSaldo(double x){
-        return saldo;
-    }
-    public static double levantar(){
-        return 0;
-    }
-    public static double depositar(){
-        return 0;
-    }
-
-    @Override
-    public String toString() {
-        return "ContaAPrazo{" + "nome=" + nome + ", saldo=" + saldo + '}';
-    }
+   
     
     
 }
--- CursoJava02e-08/src/metodos/Ex1/TesteConta.java
+++ CursoJava02e-08/src/metodos/Ex1/TesteConta.java
@@ -7,8 +7,17 @@ package metodos.Ex1;
 public class TesteConta {
     public static void main(String[] args) {
         ContaAPrazo conta = new ContaAPrazo();
-        conta.definirSaldo(5000);
-        System.out.println(conta.toString());
+        conta.proprietario = "bbbb";
+        conta.saldo = 500;
+        
+        //veriificando saldo atual
+        System.out.println("O saldo atual e de "+conta.saldo);
+        //Leavantamento
+        conta.levantamento(250);
+        System.out.println(conta.saldo);
+        // deposito
+        conta.deposita(100);
+        System.out.println(conta.saldo);
     }
     
 }
