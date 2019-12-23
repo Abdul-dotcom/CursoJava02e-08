a07f891969dfa954eb50cffbe12ebf5353fb8706
Abdul-dotcom <Abdulrazak521.ar@gmail.com>
2019-12-23 13:01:10 +0200

    Resolucao do exercicio 7 sobre recursividade

---
+++ CursoJava02e-08/src/recursividade/exer7/ArrayReais.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package recursividade.exer7;
+
+/**
+ *
+ * @author _AlyyRazack
+ */
+public class ArrayReais {
+    int somaArray(int v[],int n){
+        if (n == 0) return 0;
+        return v[n-1] +soma(v,n-1);
+    }
+    
+}
