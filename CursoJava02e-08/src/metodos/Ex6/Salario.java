7485ad3b957480a8114315da87c50514347a350d
Abdul-dotcom <Abdulrazak521.ar@gmail.com>
2019-12-23 13:36:15 +0200

    Resolucao do exercicio 6 sobre recursividade

---
+++ CursoJava02e-08/src/metodos/Ex6/Salario.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package metodos.Ex6;
+
+/**
+ *
+ * @author _AlyyRazack
+ */
+public class Salario {
+    public static void main(String[] args) {
+        
+    }
+    
+}
