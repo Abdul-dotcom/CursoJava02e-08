4ee383f857f9d911242b5fa82b97460deb6d0330
Abdul-dotcom <Abdulrazak521.ar@gmail.com>
2019-12-23 13:51:21 +0200

    update exercicio 1(Classe funcionario)

--- CursoJava02e-08/src/heranca/exer1/Funcionario.java
+++ CursoJava02e-08/src/heranca/exer1/Funcionario.java
@@ -10,5 +10,58 @@ package heranca.exer1;
  * @author _AlyyRazack
  */
 public class Funcionario {
+    private String nome;
+    private String codigo;
+    private String Cargo;
+    private double salario;
+
+    public Funcionario() {
+    }
+
+    public Funcionario(String nome, String codigo, String Cargo, double salario) {
+        this.nome = nome;
+        this.codigo = codigo;
+        this.Cargo = Cargo;
+        this.salario = salario;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getCargo() {
+        return Cargo;
+    }
+
+    public void setCargo(String Cargo) {
+        this.Cargo = Cargo;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    @Override
+    public String toString() {
+        return "Funcionario{" + "nome=" + nome + ", codigo=" + codigo + ", Cargo=" + Cargo + ", salario=" + salario + '}';
+    }
+    
+    
     
 }
